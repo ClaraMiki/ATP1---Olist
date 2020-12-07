93e9a699fc960e6f66154bbb6ed9413999d450d1
Clara Baumgarten <boclara@hotmail.com>
2020-12-07 17:48:13 -0300

    Foi acrescentado menu de filtros para pesquisa

--- main.py
+++ main.py
@@ -20,13 +20,66 @@ while choice_option != 5:
         if len(products) == 0:
            print("\n\nNão há produtos cadastrados ainda!")
         else:
-            print("\n\n########### LISTAGEM DE PRODUTOS ###########\n\n")
-            for product in products:
-                product.show_all()
-                print("------------------------------------\n")
+            print("\n\n########### SUB MENU LISTAGEM ###########\n")
+            print("1 - Sem filtro")
+            print("2 - Com filtro")
+            choice_option = int(input("Digite a sua escolha: "))
+
+            if choice_option == 1:
+                print("\n\n########### LISTAGEM DE PRODUTOS ###########\n")
+                for product in products:
+                    product.show_all()
+                    print("------------------------------------\n")
+            else:
+                print("\n\n########### SUB MENU FILTROS ###########\n")
+                print("0 - Posição")
+                print("1 - Nome")
+                print("2 - Preço")
+                print("3 - Quantidade")
+                print("4 - Descrição")
+                choice_filter = int(input("Digite a sua escolha: "))
+
+                if choice_filter == 0:
+                    filter_position = int(input("\nFiltre por posição: "))
+                    if filter_position > len(products) or filter_position < 0:
+                        print("\n\n########### ERRO ###########")
+                        print("     Posição inexistente     ")
+                        print("############################\n\n")
+                    else:
+                        print("\n########### FILTRO - Posição: ", filter_position, " ###########\n")
+                        products[filter_position].show_all()
+                elif choice_filter == 1:
+                    filter_name = input("\nFiltre por nome: ")
+                    print("\n########### FILTRO - Nome: ", filter_name, " ###########\n")
+                    for product in products:
+                        if product.get_name() == filter_name:
+                            product.show_all()
+                            print("------------------------------------\n")
+                elif choice_filter == 2:
+                    filter_price = input("\nFiltre por preço: ")
+                    print("\n########### FILTRO - Preço: ", filter_price, " ###########\n")
+                    for product in products:
+                        if product.get_price() == filter_price:
+                            product.show_all()
+                            print("------------------------------------\n")
+                elif choice_filter == 2:
+                    filter_quantity = input("\nFiltre por quantidade: ")
+                    print("\n########### FILTRO - Quantidade: ", filter_quantity, " ###########\n")
+                    for product in products:
+                        if product.get_quantity() == filter_quantity:
+                            product.show_all()
+                            print("------------------------------------\n")
+                elif choice_filter == 2:
+                    filter_description = input("\nFiltre por descrição: ")
+                    print("\n########### FILTRO - Descrição: ", filter_description, " ###########\n")
+                    for product in products:
+                        if product.get_description() == filter_description:
+                            product.show_all()
+                            print("------------------------------------\n")
+                
 
     elif choice_option == 2:
-        print("\n\n########### CADASTRO DE UM NOVO PRODUTO ###########")
+        print("\n\n########### CADASTRO DE UM NOVO PRODUTO ###########\n")
         product_name = input("Digite o nome do produto: ")
         product_price = input("Digite o preço do produto: ") 
         product_quantity = input("Digite a quantidade de estoque do produto: ")
@@ -38,32 +91,37 @@ while choice_option != 5:
         if len(products) == 0:
             print("\n\nNão há produtos cadastrados ainda!")
         else:
-            print("\n\n########### ALTERAÇÃO DE PRODUTO ###########")
+            print("\n\n########### ALTERAÇÃO DE PRODUTO ###########\n")
             product_position = int(input("Digite a posição do produto que deseja alterar: "))
-            products[product_position].show_all()
+            if product_position > len(products) or product_position < 0:
+                print("\n\n########### ERRO ###########")
+                print("     Posição inexistente     ")
+                print("############################\n\n")
+            else:
+                products[product_position].show_all()
 
-            name = input("Digite o novo nome: ")
-            price = input("Digite o novo preço: ")
-            quantity = input("Digite a nova quantidade: ")
-            description = input("Digite a nova descrição: ")
+                name = input("Digite o novo nome: ")
+                price = input("Digite o novo preço: ")
+                quantity = input("Digite a nova quantidade: ")
+                description = input("Digite a nova descrição: ")
 
-            products[product_position].set_name(name)
-            products[product_position].set_price(price)
-            products[product_position].set_quantity(quantity)
-            products[product_position].set_description(description)
+                products[product_position].set_name(name)
+                products[product_position].set_price(price)
+                products[product_position].set_quantity(quantity)
+                products[product_position].set_description(description)
 
-            print("\n\nAlteração feita com sucesso!\n\n")
+                print("\n\nAlteração feita com sucesso!\n\n")
     elif choice_option == 4:
         if len(products) == 0:
             print("\n\nNão há produtos cadastrados ainda!")
         else:
-            print("\n\n########### EXCLUSÃO DE PRODUTO ###########")
+            print("\n\n########### EXCLUSÃO DE PRODUTO ###########\n")
             product_position = int(input("Digite a posição do produto que deseja excluir: "))
 
-            if product_position > len(products):
+            if product_position > len(products) or product_position < 0:
                 print("\n\n########### ERRO ###########")
                 print("     Posição inexistente     ")
                 print("############################\n\n")
             else:
                 products.pop(product_position)
-                print("Excluído com sucesso!\n")
+                print("\nExcluído com sucesso!\n")
